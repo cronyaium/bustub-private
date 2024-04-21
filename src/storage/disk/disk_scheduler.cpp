4e610e2faa245b7b7c4674449271ddc7a0e4f5bc
cronyaium <cronyaium@gmail.com>
2024-04-21 18:52:18 +0800

    complete and pass project 1: task 2

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "storage/disk/disk_scheduler.h"
+#include <optional>
 #include "common/exception.h"
 #include "storage/disk/disk_manager.h"
 
@@ -18,10 +19,6 @@ namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
   // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
-
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
 }
@@ -34,8 +31,26 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  std::optional<DiskRequest> it = std::move(r);
+  request_queue_.Put(std::move(it));
+}
 
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  while (disk_manager_ != nullptr) {
+    auto opt = request_queue_.Get();
+    if (opt.has_value()) {
+      if (opt->is_write_) {
+        disk_manager_->WritePage(opt->page_id_, opt->data_);
+      } else {
+        disk_manager_->ReadPage(opt->page_id_, opt->data_);
+      }
+      opt->callback_.set_value(true);
+    } else {
+      // receive std::nullopt
+      break;
+    }
+  }
+}
 
 }  // namespace bustub
