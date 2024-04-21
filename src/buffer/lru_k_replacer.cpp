81521b2c4c44757d12fc08f7782b5f6f5bbc1e02
cronyaium <cronyaium@gmail.com>
2024-04-21 12:26:23 +0800

    complete and pass project 1: task 1

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -17,14 +17,76 @@ namespace bustub {
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::scoped_lock<std::mutex> lock(latch_);
+  if (evict_.empty()) {
+    return false;
+  }
+  size_t max_timestamp = 0;
+  size_t front_timestamp = SIZE_MAX;
+  frame_id_t ret_frameid = 0;
+  for (const auto &fid : evict_) {
+    auto node = node_store_.at(fid);
+    auto [front, timestamp] = node.Calculate(current_timestamp_);
+    if (timestamp > max_timestamp) {
+      max_timestamp = timestamp;
+      ret_frameid = fid;
+      front_timestamp = front;
+    } else if (timestamp == max_timestamp && front_timestamp > front) {
+      ret_frameid = fid;
+      front_timestamp = front;
+    }
+  }
+  evict_.erase(ret_frameid);
+  node_store_.erase(ret_frameid);
+  *frame_id = ret_frameid;
+  return true;
+}
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  BUSTUB_ASSERT(static_cast<size_t>(frame_id) <= replacer_size_, "Error: frame_id greater than replacer_size_");
+  // 1. not in map, then create mapping
+  if (node_store_.find(frame_id) == node_store_.end()) {
+    node_store_[frame_id] = LRUKNode(k_, frame_id);
+  }
+  // 2. Record Access
+  node_store_[frame_id].Record(current_timestamp_);
+  unevict_.insert(frame_id);
+  // 3. update current timestamp
+  ++current_timestamp_;
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  if (node_store_.find(frame_id) != node_store_.end()) {
+    auto &node = node_store_[frame_id];
+    auto now_evict = node.IsEvictable();
+    if (now_evict != set_evictable) {
+      node.SetEvict(set_evictable);
+      if (now_evict) {
+        evict_.erase(frame_id);
+        unevict_.insert(frame_id);
+      } else {
+        evict_.insert(frame_id);
+        unevict_.erase(frame_id);
+      }
+    }
+  }
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(latch_);
+  if (node_store_.find(frame_id) != node_store_.end()) {
+    node_store_.erase(frame_id);
+    evict_.erase(frame_id);
+    unevict_.erase(frame_id);
+  }
+}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+auto LRUKReplacer::Size() -> size_t {
+  std::scoped_lock<std::mutex> lock(latch_);
+  return evict_.size();
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -12,10 +12,12 @@
 
 #pragma once
 
+#include <cstdint>
 #include <limits>
 #include <list>
 #include <mutex>  // NOLINT
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 #include "common/config.h"
@@ -30,10 +32,29 @@ class LRUKNode {
   /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
   // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
 
-  [[maybe_unused]] std::list<size_t> history_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] frame_id_t fid_;
-  [[maybe_unused]] bool is_evictable_{false};
+  std::list<size_t> history_;
+  size_t k_;
+  frame_id_t fid_;
+  bool is_evictable_{false};
+
+ public:
+  explicit LRUKNode(size_t k = 0, frame_id_t fid = 0) : k_(k), fid_(fid) {}
+  auto IsEvictable() const -> bool { return is_evictable_; }
+  auto GetFrameId() const -> frame_id_t { return fid_; }
+  void SetEvict(bool is_evictable) { is_evictable_ = is_evictable; }
+  // assume history_ is not empty
+  auto Calculate(const size_t &current_timestamp) const -> std::pair<size_t, size_t> {
+    if (history_.size() < k_) {
+      return {history_.front(), SIZE_MAX};
+    }
+    return {history_.front(), current_timestamp - history_.front()};
+  }
+  void Record(const size_t &timestamp) {
+    if (history_.size() == k_) {
+      history_.pop_front();
+    }
+    history_.push_back(timestamp);
+  }
 };
 
 /**
@@ -150,12 +171,12 @@ class LRUKReplacer {
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] std::mutex latch_;
+  std::unordered_map<frame_id_t, LRUKNode> node_store_;
+  size_t current_timestamp_{0};
+  size_t replacer_size_;
+  size_t k_;
+  std::mutex latch_;
+  std::unordered_set<frame_id_t> evict_, unevict_;
 };
 
 }  // namespace bustub
