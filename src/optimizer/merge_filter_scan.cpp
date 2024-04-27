ec15c888715fb83a61a3eab8fd71b65a042b5754
cronyaium <cronyaium@gmail.com>
2024-04-27 21:04:02 +0800

    fix errors in project 3: task 1

--- src/optimizer/merge_filter_scan.cpp
+++ src/optimizer/merge_filter_scan.cpp
@@ -5,11 +5,8 @@
 #include "execution/plans/seq_scan_plan.h"
 #include "execution/plans/sort_plan.h"
 #include "execution/plans/topn_plan.h"
-#include "execution/plans/index_scan_plan.h"
 
 #include "optimizer/optimizer.h"
-#include "execution/expressions/column_value_expression.h"
-#include "execution/expressions/comparison_expression.h"
 
 namespace bustub {
 
@@ -28,29 +25,9 @@ auto Optimizer::OptimizeMergeFilterScan(const AbstractPlanNodeRef &plan) -> Abst
     if (child_plan.GetType() == PlanType::SeqScan) {
       const auto &seq_scan_plan = dynamic_cast<const SeqScanPlanNode &>(child_plan);
       if (seq_scan_plan.filter_predicate_ == nullptr) {
-        auto comp = dynamic_cast<const ComparisonExpression *>(filter_plan.GetPredicate().get());
-        if (comp != nullptr) {
-          auto lhs = dynamic_cast<const ColumnValueExpression*>(comp->children_[0].get());
-          auto rhs = dynamic_cast<ConstantValueExpression*>(comp->children_[1].get());
-          if (lhs != nullptr && rhs != nullptr) {
-            auto index_info = MatchIndex(seq_scan_plan.table_name_, lhs->GetColIdx());
-            if (index_info != std::nullopt) {
-              return std::make_shared<IndexScanPlanNode>(seq_scan_plan.output_schema_, seq_scan_plan.table_oid_, std::get<0>(index_info.value()), filter_plan.GetPredicate(), rhs);
-            }
-          }
-          auto lhs2 = dynamic_cast<const ColumnValueExpression*>(comp->children_[1].get());
-          auto rhs2 = dynamic_cast<ConstantValueExpression*>(comp->children_[0].get());
-          if (lhs2 != nullptr && rhs2 != nullptr) {
-            auto index_info = MatchIndex(seq_scan_plan.table_name_, lhs2->GetColIdx());
-            if (index_info != std::nullopt) {
-              return std::make_shared<IndexScanPlanNode>(seq_scan_plan.output_schema_, seq_scan_plan.table_oid_, std::get<0>(index_info.value()), filter_plan.GetPredicate(), rhs2);
-            }
-          }
-        }
         return std::make_shared<SeqScanPlanNode>(filter_plan.output_schema_, seq_scan_plan.table_oid_,
                                                  seq_scan_plan.table_name_, filter_plan.GetPredicate());
       }
-      
     }
   }
 
--- src/optimizer/seqscan_as_indexscan.cpp
+++ src/optimizer/seqscan_as_indexscan.cpp
@@ -1,11 +1,45 @@
 #include "optimizer/optimizer.h"
+#include "execution/plans/index_scan_plan.h"
+#include "execution/plans/seq_scan_plan.h"
+#include "execution/plans/filter_plan.h"
+#include "execution/expressions/column_value_expression.h"
+#include "execution/expressions/comparison_expression.h"
 
 namespace bustub {
 
 auto Optimizer::OptimizeSeqScanAsIndexScan(const bustub::AbstractPlanNodeRef &plan) -> AbstractPlanNodeRef {
   // TODO(student): implement seq scan with predicate -> index scan optimizer rule
   // The Filter Predicate Pushdown has been enabled for you in optimizer.cpp when forcing starter rule
-  return plan;
+  std::vector<AbstractPlanNodeRef> children;
+  for (const auto &child : plan->GetChildren()) {
+    children.emplace_back(OptimizeSeqScanAsIndexScan(child));
+  }
+
+  auto optimized_plan = plan->CloneWithChildren(std::move(children));
+  if (optimized_plan->GetType() == PlanType::SeqScan) {
+    const auto &seq_scan_plan = dynamic_cast<const SeqScanPlanNode &>(*optimized_plan);
+    auto comp = dynamic_cast<const ComparisonExpression *>(seq_scan_plan.filter_predicate_.get());
+    if (comp != nullptr) {
+      auto lhs = dynamic_cast<const ColumnValueExpression*>(comp->children_[0].get());
+      auto rhs = dynamic_cast<ConstantValueExpression*>(comp->children_[1].get());
+      if (lhs != nullptr && rhs != nullptr) {
+        auto index_info = MatchIndex(seq_scan_plan.table_name_, lhs->GetColIdx());
+        if (index_info != std::nullopt) {
+          return std::make_shared<IndexScanPlanNode>(seq_scan_plan.output_schema_, seq_scan_plan.table_oid_, std::get<0>(index_info.value()), seq_scan_plan.filter_predicate_, rhs);
+        }
+      }
+      auto lhs2 = dynamic_cast<const ColumnValueExpression*>(comp->children_[1].get());
+      auto rhs2 = dynamic_cast<ConstantValueExpression*>(comp->children_[0].get());
+      if (lhs2 != nullptr && rhs2 != nullptr) {
+        auto index_info = MatchIndex(seq_scan_plan.table_name_, lhs2->GetColIdx());
+        if (index_info != std::nullopt) {
+          return std::make_shared<IndexScanPlanNode>(seq_scan_plan.output_schema_, seq_scan_plan.table_oid_, std::get<0>(index_info.value()), seq_scan_plan.filter_predicate_, rhs2);
+        }
+      }
+    }
+  }
+  
+  return optimized_plan;
 }
 
 }  // namespace bustub
