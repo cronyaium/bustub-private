49c829939e0801cb87a4a4e39b4f0343f39b8db2
cronyaium <cronyaium@gmail.com>
2024-04-29 22:48:51 +0800

    complete and pass project 4: task 1

--- src/concurrency/transaction_manager.cpp
+++ src/concurrency/transaction_manager.cpp
@@ -43,6 +43,8 @@ auto TransactionManager::Begin(IsolationLevel isolation_level) -> Transaction * 
   txn_map_.insert(std::make_pair(txn_id, std::move(txn)));
 
   // TODO(fall2023): set the timestamps here. Watermark updated below.
+  // this txn can read anything affected by any commited txn
+  txn_ref->read_ts_ = last_commit_ts_.load();
 
   running_txns_.AddTxn(txn_ref->read_ts_);
   return txn_ref;
@@ -72,6 +74,7 @@ auto TransactionManager::Commit(Transaction *txn) -> bool {
   std::unique_lock<std::shared_mutex> lck(txn_map_mutex_);
 
   // TODO(fall2023): set commit timestamp + update last committed timestamp here.
+  txn->commit_ts_ = ++last_commit_ts_;
 
   txn->state_ = TransactionState::COMMITTED;
   running_txns_.UpdateCommitTs(txn->commit_ts_);
