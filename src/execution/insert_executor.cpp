b7ca7dc4110fdf545095742945d2ee82e2bb2b6e
cronyaium <cronyaium@gmail.com>
2024-05-02 14:58:09 +0800

    complete and pass project 4: task 4.1

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -30,19 +30,42 @@ auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
     RID child_rid;
     int32_t num = 0;
     while (child_executor_->Next(&child_tuple, &child_rid)) {
+      bool flag = true;
+      auto table_indexes = exec_ctx_->GetCatalog()->GetTableIndexes(table_info->name_);
+      for (auto &idx_info : table_indexes) {
+        auto &index = idx_info->index_;
+        // std::cerr << index->ToString() << std::endl;
+        if (index->GetMetadata()->IsPrimaryKey()) {
+          auto key = child_tuple.KeyFromTuple(table_info->schema_, idx_info->key_schema_, index->GetKeyAttrs());
+
+          // auto val = key.GetValue(&idx_info->key_schema_, 0);
+          std::vector<RID> result;
+          index->ScanKey(key, &result, exec_ctx_->GetTransaction());
+          if (!result.empty()) {
+            flag = false;
+            break;
+          }
+        }
+      }
+      if (!flag) {
+        exec_ctx_->GetTransaction()->SetTainted();
+        throw ExecutionException("Error: detect write-write conflict");
+      }
+
       /** Meta: the ts / txn_id of this tuple. In project 3, simply set it to 0. */
       auto ret = table->InsertTuple({exec_ctx_->GetTransaction()->GetTransactionTempTs(), false}, child_tuple,
                                     exec_ctx_->GetLockManager(), exec_ctx_->GetTransaction(), plan_->GetTableOid());
       if (ret != std::nullopt) {
         num++;
-        /*
-        auto table_indexes = exec_ctx_->GetCatalog()->GetTableIndexes(table_info->name_);
         for (auto &idx_info : table_indexes) {
           auto &index = idx_info->index_;
-          index->InsertEntry(child_tuple.KeyFromTuple(table_info->schema_, idx_info->key_schema_, index->GetKeyAttrs()),
-                             *ret, exec_ctx_->GetTransaction());
+          if (!index->InsertEntry(
+                  child_tuple.KeyFromTuple(table_info->schema_, idx_info->key_schema_, index->GetKeyAttrs()), *ret,
+                  exec_ctx_->GetTransaction())) {
+            exec_ctx_->GetTransaction()->SetTainted();
+            throw ExecutionException("Error: detect write-write conflict");
+          }
         }
-        */
         exec_ctx_->GetTransaction()->AppendWriteSet(plan_->GetTableOid(), ret.value());
       }
     }
